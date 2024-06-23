7764fddfc692b1586c070fed55020e7a0a2f9863
zeenat <zeenu.23@gmail.com>
2024-06-24 01:05:17 +0530

    add state variables for email and password
    update TextField components to use state, and modify handleSubmit function to prevent default form submission and log email value

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,13 @@
 import { Box, Card, CardContent, Container, Typography, TextField, Button } from '@mui/material'
+import { useState } from 'react'
 
 function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    function handleSubmit() {
-        
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log('Email:', email)
     }
 
   return (
@@ -15,9 +19,9 @@ function Login() {
                Login     
             </Typography>
             <Box component='form' onSubmit={handleSubmit} sx={{ mt: 1, width: '100%' }}>
-               <TextField margin='normal' label='Email' fullWidth required/>           
-               <TextField margin='normal' label='Password' fullWidth required/> 
-               <Button variant='contained'>Login</Button>      
+               <TextField margin='normal' label='Email' id='email' value={email} onChange={(e) => setEmail(e.target.value)}fullWidth required/>           
+               <TextField margin='normal' label='Password' id='password' value={password} fullWidth required/> 
+               <Button type='submit' variant='contained'>Login</Button>      
             </Box>          
           </CardContent>        
         </Card>    
